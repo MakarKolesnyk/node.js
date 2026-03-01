6c50c326d47cdf8d981e0c8b0cdbc110e4b14224
Makar <kolesnykmakar0@gmail.com>
2026-03-01 04:28:44 +0200

    Add cowsay

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+import cowsay from 'cowsay'
 import Article from "./Article.mjs";
 import { mult,del,sum} from "./math.mjs";
 
+console.log(cowsay.say({text: 'qwerty'}));
+
+
 const num1 = 5;
 const num2 = 8;
-
-console.log((sumnum1,num2));
+console.log(sum(num1,num2));
 console.log(mult(4, 9));
 console.log(del(45, 9));
 
